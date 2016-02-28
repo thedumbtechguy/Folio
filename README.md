1e4c97b8b44a9c2e164243265ab601a20f7646f6
frostymarvelous <sfroelich01@gmail.com>
2016-02-28 13:50:31 +0000

    Fix gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# Folio
-A replacement for Android Fragments with a simplified lifecycle and easy transition animations.
--- folio/src/main/java/com/umaplay/folio/PageStackDelegate.java
+++
@@ -1,9 +0,0 @@
-package com.umaplay.folio;
-
-/**
- * Interface for "finishing" a navigation stack. The intended implementation is calling finish()
- * in the host Activity.
- */
-public interface PageStackDelegate {
-    void onStackEmpty();
-}
